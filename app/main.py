ce1792230d88eb2749c033978e2978a9d6b81288
Gaurav Singh <gauravsingh428826@gmail.com>
2026-02-05 15:44:12 +0530

    Update default PORT in main.py and add Python version file

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8967))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, log_level="info")
